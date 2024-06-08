040546ee10a7bebb103eacd51e6871d475daa45c
ayush pradhan <ayushpradhan588@gmail.com>
2024-06-08 23:38:05 +0545

    cjhanges

--- controller/visitorController.js
+++ controller/visitorController.js
@@ -2,20 +2,43 @@ const { PrismaClient } = require('@prisma/client');
 const multer = require('multer');
 const prisma = new PrismaClient();
 const fs = require('fs');
-const dayjs = require('dayjs')
+const dayjs = require('dayjs');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: 'files/', // Directory where uploaded files will be stored
   filename: (req, file, cb) => {
-   // Generate a unique filename with timestamp
-   const timestamp = Date.now();
-   const filename = `${timestamp}-${file.originalname}`;
-   cb(null, filename);
+    // Generate a unique filename with timestamp
+    const timestamp = Date.now();
+    const filename = `${timestamp}-${file.originalname}`;
+    cb(null, filename);
   },
 });
 
 const upload = multer({ storage: storage }); // Use.array to handle multiple files
 
+const serveFile = async (req, res) => {
+  const { fileName } = req.params;
+  const filePath = path.join(__dirname, '..', 'files', fileName); // Assuming files are stored in the 'files' directory
+
+  try {
+    // Check if the file exists
+    if (!fs.existsSync(filePath)) {
+      return res.status(404).json({ error: 'File not found' });
+    }
+
+    // Set the appropriate headers for the response
+    res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
+    res.setHeader('Content-Type', 'application/octet-stream');
+
+    // Stream the file to the response
+    const fileStream = fs.createReadStream(filePath);
+    fileStream.pipe(res);
+  } catch (error) {
+    console.error('Error serving file:', error);
+    res.status(500).json({ error: 'Failed to serve file' });
+  }
+};
 
 const fetchVisitors = async (req, res) => {
   try {
@@ -33,28 +56,32 @@ const createVisitor = async (req, res) => {
     upload.array('files')(req, res, async (err) => {
       if (err) {
         console.error(err);
-        return res.status(500).json({ message: 'File upload error', error: err.message });
+        return res
+          .status(500)
+          .json({ message: 'File upload error', error: err.message });
       }
 
       // After successful file upload, proceed with your logic
       const body = req.body; // Access the request body
       const files = req.files; // Now contains an array of uploaded files
       body.uploaded_files = files;
-      body.status = "C";
+      body.status = 'C';
       body.check_in_time = dayjs(body.check_in_time).toISOString();
       const visitor = await prisma.visitor.create({
         data: body,
       });
       // Send a response indicating success
-      res.status(200).json({ message: 'Visitor created successfully', files,visitor });
+      res
+        .status(200)
+        .json({ message: 'Visitor created successfully', files, visitor });
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'An error occurred', error: error.message });
+    res
+      .status(500)
+      .json({ message: 'An error occurred', error: error.message });
   }
 };
-
-
 
 const deleteVisitor = async (req, res) => {
   const { visitorId } = req.params;
@@ -72,21 +99,38 @@ const deleteVisitor = async (req, res) => {
   }
 };
 
-const  updateVisitor = async (req, res) => {
-  const { visitorId } = req.params;
-  const updatedVisitorData = req.body;
-
+const updateVisitor = async (req, res) => {
   try {
-    const updatedVisitor = await prisma.visitor.update({
-      where: { id: parseInt(visitorId) },
-      data: updatedVisitorData,
-    });
+    upload.array('files')(req, res, async (err) => {
+      if (err) {
+        console.error(err);
+        return res
+          .status(500)
+          .json({ message: 'File upload error', error: err.message });
+      }
+      const { visitorId } = req.params;
+      const updatedVisitorData = req.body;
+      const files = req.files;
+      console.log(files);
+      console.log(updatedVisitorData);
+      updatedVisitorData.uploaded_files = files;
+      const updatedVisitor = await prisma.visitor.update({
+        where: { id: parseInt(visitorId) },
+        data: updatedVisitorData,
+      });
 
-    res.status(200).json(updatedVisitor);
+      res.status(200).json(updatedVisitor);
+    });
   } catch (error) {
     console.error('Error updating visitor:', error);
     res.status(500).json({ error: 'Failed to update visitor' });
   }
-}
+};
 
-module.exports = { fetchVisitors, createVisitor, deleteVisitor, updateVisitor };
+module.exports = {
+  fetchVisitors,
+  createVisitor,
+  deleteVisitor,
+  updateVisitor,
+  serveFile,
+};
--- routes/visitorRoute.js
+++ routes/visitorRoute.js
@@ -6,11 +6,13 @@ const {
   createVisitor,
   deleteVisitor,
   updateVisitor,
+  serveFile,
 } = require('../controller/visitorController');
 
 router.get('/fetchVisitors', fetchVisitors);
 router.post('/createVisitor', createVisitor);
 router.delete('/deleteVisitor/:visitorId', deleteVisitor);
-router.put('/updateVisitor/:visitorId',updateVisitor)
+router.put('/updateVisitor/:visitorId', updateVisitor);
+router.get('/file/:fileName', serveFile);
 
 module.exports = router;
